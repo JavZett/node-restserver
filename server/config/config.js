8f9c45243644dadea34a61f5a5d68b2f06d20ea5
ElMariachi <zacag1500@gmail.com>
2020-08-17 14:18:26 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mariachi:lUTktX4G0ZkxPOOw@cluster0.gvzxn.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.urlDB = urlDB;
